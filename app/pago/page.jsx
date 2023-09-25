21e840b8cabed101e8f496292311347538259e3a
Juan Cruz Musi <141969074+JuanMusi@users.noreply.github.com>
2023-09-25 18:17:40 -0300

    Añadir pagar y modificación de la pagina de pagos

--- app/pago/page.jsx
+++ app/pago/page.jsx
@@ -1,6 +1,7 @@
 import Footer from "@/app/components/Footer";
 import Header from "@/app/components/Header";
 import SideBar from "@/app/components/Sidebar";
+import Link from "next/link";
 
 const Pagos = () => {
   return (
@@ -8,13 +9,18 @@ const Pagos = () => {
       <Header />
       <SideBar />
       <h1 className="flex justify-center my-6 text-4xl">Pagos</h1>
+      <div className="flex justify-center my-6 text-1xl">
+        <Link href="/pagar" >
+          <button className="bg-orange-400 p-4 rounded-2xl text-white">Realizar un pago</button>
+        </Link>  
+      </div>
       <section className="relative overflow-x-auto border rounded-md mt-4 mx-auto w-4/5">
         <table className="w-full text-sm text-left text-gray-500">
           {/* Titulos */}
           <thead className="text-xs text-gray-700 uppercase">
             <tr>
               <th scope="col" className="px-6 py-3 bg-gray-50 font-extrabold">
-                N° de transacción
+                N° de factura
               </th>
               <th scope="col" className="px-6 py-3 font-extrabold">
                 Cuenta
@@ -23,41 +29,41 @@ const Pagos = () => {
                 Motivo
               </th>
               <th scope="col" className="px-6 py-3 font-extrabold">
-                Fecha
+                Vencimiento
               </th>
               <th scope="col" className="px-6 py-3 bg-gray-50 font-extrabold">
-                Importe
+                Importe a abonar
               </th>
             </tr>
           </thead>
           {/* Datos */}
           <tbody>
             <tr className="border-b border-gray-200 ">
-              <td className="px-6 py-4 font-medium bg-gray-50">00000000001</td>
+              <td className="px-6 py-4 font-medium bg-gray-50">FCC-0000000001</td>
               <td className="px-6 py-4">ITBA</td>
-              <td className="px-6 py-4 bg-gray-50 ">Cuota</td>
-              <td className="px-6 py-4">03/08/23</td>
+              <td className="px-6 py-4 bg-gray-50 ">Cuota Full Stack Developer</td>
+              <td className="px-6 py-4">15/10/23</td>
               <td className="px-6 py-4 bg-gray-50 ">$40.000</td>
             </tr>
             <tr className="border-b border-gray-200 ">
-              <td className="px-6 py-4 font-medium bg-gray-50">00000000001</td>
-              <td className="px-6 py-4">ITBA</td>
-              <td className="px-6 py-4 bg-gray-50 ">Cuota</td>
-              <td className="px-6 py-4">05/09/23</td>
-              <td className="px-6 py-4 bg-gray-50">$40.000</td>
+              <td className="px-6 py-4 font-medium bg-gray-50">FCC-0000000002</td>
+              <td className="px-6 py-4">Edenor</td>
+              <td className="px-6 py-4 bg-gray-50 ">Luz mes septiembre</td>
+              <td className="px-6 py-4">30/10/23</td>
+              <td className="px-6 py-4 bg-gray-50">$32.000</td>
             </tr>
             <tr className="border-b border-gray-200 ">
-              <td className="px-6 py-4 font-medium bg-gray-50">00000000001</td>
-              <td className="px-6 py-4">ITBA</td>
-              <td className="px-6 py-4 bg-gray-50 ">Cuota</td>
-              <td className="px-6 py-4">06/10/23</td>
-              <td className="px-6 py-4 bg-gray-50">$40.000</td>
+              <td className="px-6 py-4 font-medium bg-gray-50">FCB-0034521413</td>
+              <td className="px-6 py-4">Adidas</td>
+              <td className="px-6 py-4 bg-gray-50 ">Cuota n°4 compra #34521413</td>
+              <td className="px-6 py-4">06/11/23</td>
+              <td className="px-6 py-4 bg-gray-50">$17.500</td>
             </tr>
             <tr className="border-b border-gray-200 ">
-              <td className="px-6 py-4 font-medium bg-gray-50">00000000001</td>
+              <td className="px-6 py-4 font-medium bg-gray-50">FCC-0000000004</td>
               <td className="px-6 py-4">ITBA</td>
-              <td className="px-6 py-4 bg-gray-50 ">Cuota</td>
-              <td className="px-6 py-4">03/11/23</td>
+              <td className="px-6 py-4 bg-gray-50 ">Cuota Full Stack Developer</td>
+              <td className="px-6 py-4">15/11/23</td>
               <td className="px-6 py-4 bg-gray-50">$40.000</td>
             </tr>
           </tbody>
